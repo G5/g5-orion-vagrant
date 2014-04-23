a96795c429c34b1d6b826e690ae3a610890de038
Jessica Lynn Suttles <jlsuttles@gmail.com>
2014-04-23 10:59:35 -0700

    Do not set garden URLs with magic_shell
    .env and .env.development are not overriding it, so best to leave the
    setting of these to those files.

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -52,18 +52,6 @@ magic_shell_environment "G5_CONFIGURATOR_WEBHOOK_URL" do
   value "http://g5-configurator.herokuapp.com/consume_feed"
 end
 
-magic_shell_environment "LAYOUT_GARDEN_URL" do
-  value "http://g5-layout-garden.herokuapp.com/"
-end
-
-magic_shell_environment "THEME_GARDEN_URL" do
-  value "http://g5-theme-garden.herokuapp.com/"
-end
-
-magic_shell_environment "WIDGET_GARDEN_URL" do
-  value "http://g5-widget-garden.herokuapp.com/"
-end
-
 magic_shell_environment "HEROKU_APP_NAME" do
   value "not-a-heroku-app"
 end
