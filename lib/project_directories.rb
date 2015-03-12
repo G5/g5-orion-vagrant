63cc3ca31aeb481f91b17e444f0449d16b65056f
maeve <maeve.revels@getg5.com>
2015-03-12 15:33:12 -0700

    Refactor complicated method for finding project ruby version

--- lib/project_directories.rb
+++ lib/project_directories.rb
@@ -27,29 +27,32 @@ class ProjectDirectories
     end
 
     def ruby_version
-      if !@version && working_dir
-        version_file = File.join(working_dir, '.ruby-version')
-        gemfile = File.join(working_dir, 'Gemfile')
-
-        if File.exists?(version_file)
-          @version = File.read(version_file).strip
-        elsif File.exists?(gemfile)
-          File.open gemfile do |file|
-            file.find do |line|
-               match = line.match(/ruby ['"](.*)['"]/)
-               @version = match[1] if match
-            end
-          end
-        end
-      end
-
-      @version
+      @ruby_version ||= (rbenv_ruby_version || gemfile_ruby_version)
     end
 
     def ==(other)
       other.is_a?(Project) &&
         other.name == name &&
         other.repo == repo
+    end
+
+    private
+    def rbenv_ruby_version
+      rbenv_file = File.join(working_dir.to_s, '.ruby-version')
+      File.read(rbenv_file).strip if File.exists?(rbenv_file)
+    end
+
+    def gemfile_ruby_version
+      gemfile = File.join(working_dir.to_s, 'Gemfile')
+      read_ruby_from_gemfile(gemfile) if File.exists?(gemfile)
+    end
+
+    def read_ruby_from_gemfile(gemfile)
+      File.open gemfile do |file|
+        ruby_directive = file.find { |line| line =~ /^\s*ruby/ }
+        match = ruby_directive.to_s.match(/['"](.*)['"]\s*$/)
+        match[1] if match
+      end
     end
   end
 
